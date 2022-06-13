6c29f14138647bc36ae1d92c4ed343d972f4835e
Candace Eckels <candace.eckels@gmail.com>
2022-06-13 11:44:04 -0600

    Added tests and code for encryptor method part1

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,17 +1,23 @@
-# come back to revist how shifter can use zeros
-# this class will return the date from the shifter class if nothing is input
-# needs a from shifter class
+# this class will return the date from 
+# the shifter class if nothing is input
+
+#If key.nil? == true reset key value to new_key
 require_relative '../lib/shifter.rb'
 
 class Enigma
-    attr_reader :alpha, :shifter
+    attr_reader :alpha, :shifter, :encryption
     def initialize
         @alpha = ("a".."z").to_a << " "
         @shifter = Shifter.new
+        @encryption = {}
     end
 
-    def encryptor(message, key, date)
-        
+    def encryptor(message, key, date = shifter.date_formater)
+        new_key = shifter.keys
+        @encryption[:message] = message.downcase
+        @encryption[:key] = key
+        @encryption[:date] = date
+        @encryption
     end
 
     def decrypt(ciphertext, key, date)
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -10,6 +10,15 @@ RSpec.describe do Enigma
         expect(@enigma).to be_a Enigma
         expect(@enigma.alpha.length).to eq 27
         expect(@enigma.shifter).to be_a Shifter
+        expect(@enigma.shifter.keys.length).to eq 5
+        expect(@enigma.shifter.offsets).to be_a Date
+    end
+
+    it 'can return a Hash' do
+        expect(@enigma.encryptor("hello world",02715,1025)).to be_a Hash
+        expect(@enigma.encryption[:message]).to eq "hello world"
+        expect(@enigma.encryption[:key]).to eq 02715
+        expect(@enigma.encryption[:date]).to eq 1025
     end
 
 
